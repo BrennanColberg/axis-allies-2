56db7653b03101fe5b45726b365240ca8e095a7d
Brennan Colberg <brennan.colberg@gmail.com>
2018-11-11 16:19:08 -0800

    infantry quotient calculation

--- src/Main.java
+++ src/Main.java
@@ -3,28 +3,46 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
-	
+
 	private static Data data;
 
 	public static void main(String[] args) throws FileNotFoundException {
 		data = new Data("1942/");
 
-		System.out.println(calculateWeakness(data.territories.get("Borneo")));
-		System.out.println(calculateWeakness(data.territories.get("Borneo")));
-		System.out.println(calculateWeakness(data.territories.get("Anglo-Egypt Sudan")));
+		System.out.println(calculateInfantryQuotient(data.territories.get("Borneo")));
+		System.out.println(calculateInfantryQuotient(data.territories.get("Germany")));
+		System.out.println(calculateInfantryQuotient(data.territories.get("Anglo-Egypt Sudan")));
 
 	}
-	
-	// currently returns chance of winning with 10 infantry... should be # of infantry
-	// needed to win a majority of the time
-	private static double calculateWeakness(Territory territory) {
-		
-		// setup 10 infantry to attack
+
+	/**
+	 * Calculates the approximate number of infantry needed to conquer 50% of the
+	 * time (decimal value).
+	 * 
+	 * @param territory territory to test defenses of
+	 * @return the calculated infantry quotient
+	 */
+	private static double calculateInfantryQuotient(Territory territory) {
+		int infantry = 1;
+		double lastPercentage = 0;
+		double percentage = 0;
+		while ((percentage = calculateWinPercentage(territory, infantry)) < 0.5) {
+			lastPercentage = percentage;
+			infantry++;
+		}
+		double difference = percentage - lastPercentage;
+		double offset = 0.5 - lastPercentage;
+		return infantry - 1 + offset / difference;
+	}
+
+	private static double calculateWinPercentage(Territory territory, int infantry) {
+
+		// set up infantry to attack
 		Set<Unit> offense = new HashSet<>();
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < infantry; i++)
 			offense.add(new Unit(data.units.get("Infantry"), territory.getAlliance().other()));
 		territory.addUnits(offense);
-		
+
 		// calculate 1000 wins
 		int offenseWins = 0;
 		int runs = 1000;
@@ -34,12 +52,12 @@ public class Main {
 				offenseWins++;
 			}
 		}
-				
+
 		// retreat
 		territory.removeUnits(offense);
-		
+
 		return (double) offenseWins / runs;
-		
+
 	}
 
 }
